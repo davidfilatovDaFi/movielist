46b0885aed3a6a5b1dede38bcd3e5d88659824df
davidfilatovDaFi <oxelolol@gmail.com>
2023-04-12 18:33:40 +0300

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import logo from '../../assets/imgs/logo.png'
+import favorite from '../../assets/imgs/favorite.svg'
+import styles from './Header.module.scss'
+
+export default function Header() {
+
+  const [search,setSearch] = useState('')
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.body}>
+        <h1 className={styles.title}>Movie<span>List</span></h1>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className={styles.user}>
+        <input value={search} onChange={e => setSearch(e.target.value)} className='search' type="text" />
+        <a href=""><img src={favorite} alt="favorite" /></a>
+      </div>
+  </header>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className='container'>
+        <Header/>
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
